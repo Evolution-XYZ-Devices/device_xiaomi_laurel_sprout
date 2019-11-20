25af778afa35cc0ee653ef1d78a778a2039f84a6
Demon000 <demonsingur@gmail.com>
2019-11-20 13:25:49 +0100

    laurel_sprout: fod: Adjust nit value used based on screen brightness
    Change-Id: I4307b49917333b90356067d5333fdb4cce4c4844

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -25,7 +25,8 @@
 #define FINGERPRINT_ERROR_VENDOR 8
 
 #define COMMAND_NIT 10
-#define PARAM_NIT_FOD 3
+#define PARAM_NIT_630_FOD 1
+#define PARAM_NIT_300_FOD 4
 #define PARAM_NIT_NONE 0
 
 #define DISPPARAM_PATH "/sys/class/drm/card0-DSI-1/disp_param"
@@ -104,14 +105,14 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
-    set(FOD_HBM_PATH, FOD_HBM_ON);
-    
+    set(DISPPARAM_PATH, DISPPARAM_FOD_BACKLIGHT_HBM);
+
     if (get(BRIGHTNESS_PATH, 0) > 100) {
-         xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_300_FOD);
-     } else if (get(BRIGHTNESS_PATH, 0) != 0) {
-         xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
-     }
-    
+        xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_300_FOD);
+    } else if (get(BRIGHTNESS_PATH, 0) != 0) {
+        xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
+    }
+
     return Void();
 }
 
