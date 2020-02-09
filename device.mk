e478e6c5e6f8352e0b0c4a155ddb97d27be96a37
Frank Corona <frankdavid.corona@gmail.com>
2020-02-09 16:25:43 -0500

    laurel_sprout: Adding permissions for fingerprint from aosp vendors
    Signed-off-by: Frank Corona <frankdavid.corona@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
 
 # Fingerprint
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_trinket
 
