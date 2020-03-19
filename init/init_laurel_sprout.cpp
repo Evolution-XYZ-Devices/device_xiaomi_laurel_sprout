a302edd16db9ffe7815e7174a82ffdd0f3ba01ca
Chaplindev <frankdavid.corona@gmail.com>
2020-03-19 03:12:20 +0000

    Revert "laurel_sprout: disable a2dp offload and switch to default bt audio HAL"
    This reverts commit b03906cbe11f60306f5f22d334a1f870ae738199.

--- init/init_laurel_sprout.cpp
+++ init/init_laurel_sprout.cpp
@@ -60,10 +60,4 @@ void vendor_load_properties()
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
     property_override_dual("ro.control_privapp_permissions", "ro.control_privapp_permissions", "log");
     property_override("ro.oem_unlock_supported", "0");
-
-    property_override("ro.bluetooth.a2dp_offload.supported", "false");
-    property_override("persist.bluetooth.a2dp_offload.disabled", "true");
-    property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
-    property_override("persist.vendor.qcom.bluetooth.enable.splita2dp", "false");
-    property_override("vendor.audio.feature.a2dp_offload.enable", "false");
 }
