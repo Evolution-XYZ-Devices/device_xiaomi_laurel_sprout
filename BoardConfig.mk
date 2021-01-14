d23f754857f46255174ba6ac25d0edcbfbf252c5
Nishant Singh <nishant.singh@pixysos.com>
2021-01-14 10:39:37 +0530

    laurel_sprout: Use prebuilt kernel
    * Image.gz-dtb from No.912-OSS-Batt-20201212-2148.zip
    * DTBO.img from No.912-OSS-Batt-20201212-2148.zip
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,14 +53,15 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
-TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_VERSION := 4.14
 TARGET_KERNEL_CLANG_VERSION := proton
 KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/Image.gz-dtb
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_HEADERS := kernel/xiaomi/laurel_sprout
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
