302737d48c82ddfdc6865d56dbab6e6924cc97d1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-26 17:15:13 +0530

    laurel_sprout: SEPolicy: Adapt Q sepolicy to R SEPolicy
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,10 +202,10 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Sepolicy
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 
 # Treble
