6345a69a679606444a95b0851ba6e0cb79b22604
Magicxavi <rolfpeterkesche@gmail.com>
2021-01-31 09:00:47 +0200

    laurel_sprout: Uprev health HAL
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- device.mk
+++ device.mk
@@ -305,7 +305,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # IMS
 PRODUCT_PACKAGES += \
