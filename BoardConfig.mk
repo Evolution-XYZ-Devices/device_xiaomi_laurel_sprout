daeeba5439168a2792568696f937faaef913cbce
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-03-12 10:44:14 +0530

    laurel_sprout: SEPolicy: Cleanup
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,7 +201,6 @@ TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Shim
