5b828adf765b1c2be4111cfded3d6ed581097f57
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-04-23 21:50:47 +0530

    laurel_sprout: Let's GO ENFORCING!
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,6 @@ TARGET_SCREEN_DENSITY := 320
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=16
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
