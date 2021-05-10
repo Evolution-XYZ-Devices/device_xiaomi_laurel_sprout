ee208e92597446cd2c1cf6e97d009abaf9c6e856
Joey Huab <joey@evolution-x.org>
2021-05-11 01:05:13 +0530

    laurel_sprout: Do not enforce RRO for all resource overlays
    This reverts commit bef2cea0dd702d992426a323752d31deb02753c1.
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- device.mk
+++ device.mk
@@ -311,8 +311,6 @@ PRODUCT_PACKAGES += \
     NotchBarKiller \
     NoCutoutOverlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
