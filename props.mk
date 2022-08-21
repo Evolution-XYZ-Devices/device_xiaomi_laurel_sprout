36d3fd14164da32166334559d5e9132b09500761
weichinweng <weichinweng@google.com>
2022-08-22 03:48:42 +0530

    laurel_sprout: BT: Align ASHA prop with BluetoothProperties
    * Modify bluetooth.profile.asha.central to
      bluetooth.profile.asha.central.enabled
    
    Tag: #feature
    Test: manual
    Bug: 211570675
    Bug: 218454632
    
    Change-Id: I8e1f49034e5a675b29fba747e634c9f7c6c29c50
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- props.mk
+++ props.mk
@@ -117,7 +117,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     bluetooth.device.class_of_device=90,2,12 \
-    bluetooth.profile.asha.central=false
+    bluetooth.profile.asha.central.enabled=false
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
