2ffee9974dee1e84f4b8a2e630f21a614d6e4ce0
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-09-25 12:38:45 +0530

    Revert "laurel_sprout: props: Tune calling audio props"
    This reverts commit a50eabae0f30bf54dc6bcf78d8be18b5cf015fdb.

--- props.mk
+++ props.mk
@@ -96,7 +96,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.snd_mon.enable=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.media_vol_default=10 \
+    ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_default=5 \
     ro.config.vc_call_vol_steps=7
 
