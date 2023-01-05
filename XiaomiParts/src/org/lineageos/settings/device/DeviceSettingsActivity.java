c2509aba48b35f19b32cfa05eec6f3b04d77fe89
Bruno Martins <bgcngm@gmail.com>
2023-01-05 21:36:35 +0530

    laurel_sprout: XiaomiParts: Fix compilation for QPR1 merge
    Change-Id: I71ad7db85592ca18e3f0f8155bba5a472bfadbaa
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- XiaomiParts/src/org/lineageos/settings/device/DeviceSettingsActivity.java
+++ XiaomiParts/src/org/lineageos/settings/device/DeviceSettingsActivity.java
@@ -23,7 +23,7 @@ import android.os.Bundle;
 
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
 
